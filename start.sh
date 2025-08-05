37a38dcc9ab59e69265342faa42e14f64a9b3487
B-AJ-Amar <amarbouakaz91@gmail.com>
2025-08-05 20:51:32 +0100

    Refactor Dockerfile: streamline build process and remove startup script

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Use PORT environment variable if set, otherwise default to 7205
-PORT=${PORT:-7205}
-
-echo "Starting gTunnel server on 0.0.0.0:$PORT"
-
-# Start the server with the correct bind address
-exec ./gtunnel-server start --bind-address "0.0.0.0:$PORT"
